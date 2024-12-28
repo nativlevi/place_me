74f5aa6c8dd06d6fe9a7cd5e11bd7433c18b60c1
nativlevi <nativl12345@gmail.com>
2024-12-29 00:47:16 +0200

    changing style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:ui';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +15,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginScreen(),
         '/guide': (context) => GuideScreen(),
+        '/manager_login': (context) => ManagerLoginScreen(),
+        '/participant_login': (context) => ParticipantLoginScreen(),
+        '/manager_dashboard': (context) => ManagerDashboard(),
+        '/participant_dashboard': (context) => ParticipantDashboard(),
       },
     );
   }
@@ -36,7 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFa5bfcc),
+      backgroundColor: Color(0xFFefefef),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -59,47 +64,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  void showAuthOptions(BuildContext context, String userType) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('$userType Authentication'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Navigating to $userType Registration...')),
-                  );
-                },
-                child: Text('Register'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Navigating to $userType Login...')),
-                  );
-                },
-                child: Text('Login'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
+  void navigateToManagerLogin(BuildContext context) {
+    Navigator.pushNamed(context, '/manager_login');
   }
 
-  void navigateToManager(BuildContext context) {
-    showAuthOptions(context, 'Manager');
-  }
-
-  void navigateToParticipant(BuildContext context) {
-    showAuthOptions(context, 'Participant');
+  void navigateToParticipantLogin(BuildContext context) {
+    Navigator.pushNamed(context, '/participant_login');
   }
 
   void navigateToUserGuide(BuildContext context) {
@@ -115,8 +85,8 @@ class _LoginScreenState extends State<LoginScreen> {
             Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("images/first_screen.jpg"), // Path to your image
-                  fit: BoxFit.cover, // Adjusts the image to cover the screen
+                  image: AssetImage("images/first_screen.jpg"),
+                  fit: BoxFit.cover,
                 ),
               ),
               child: Container(
@@ -131,26 +101,11 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Container(
                   padding: EdgeInsets.all(8),
                   decoration: BoxDecoration(
-                    color: Colors.transparent, // Transparent background
+                    color: Colors.transparent,
                   ),
                   child: Icon(
                     Icons.help_outline,
-                    color: Colors.white, // White icon color
-                    size: 24.0,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 10,
-              right: 10,
-              child: GestureDetector(
-                onTap: () => navigateToUserGuide(context),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.help_outline,
-                    color: Colors.black,
+                    color: Colors.white,
                     size: 24.0,
                   ),
                 ),
@@ -160,20 +115,30 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(height: MediaQuery.of(context).padding.top), // רווח מעל התוכן
-                  Text('PlaceMe',style: TextStyle(color: Colors.white,fontSize: 100.0,fontFamily:'Satreva')),
-                  Text('Smart Seating, Perfect Placement',style: TextStyle(color: Colors.white,fontFamily: 'Source Sans 3',fontSize: 20.0)),
-                  SizedBox(height: 100.0),
+                  Text('Welcome',style: TextStyle(fontSize: 50.0,color: Colors.white,fontFamily: 'Source Sans 3')),
+                  Text('To',style: TextStyle(fontSize: 35.0,color: Colors.white,fontFamily: 'Source Sans 3')),
+                  Text('PlaceMe',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 100.0,
+                          fontFamily: 'Satreva')),
+                  Text('Smart Seating, Perfect Placement',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Source Sans 3',
+                          fontSize: 20.0)),
+                  SizedBox(height: 50.0),
+                  Text('Continue as:',style: TextStyle(color: Colors.white,fontSize: 25.0)),
+                  SizedBox(height: 15.0),
                   GestureDetector(
-                    onTap: () => navigateToManager(context),
+                    onTap: () => navigateToManagerLogin(context),
                     child: buildCustomButton('Manager   '),
                   ),
                   SizedBox(height: 10.0),
                   GestureDetector(
-                    onTap: () => navigateToParticipant(context),
+                    onTap: () => navigateToParticipantLogin(context),
                     child: buildCustomButton('Participant'),
                   ),
-                  SizedBox(height: 10.0),
                 ],
               ),
             ),
@@ -187,17 +152,17 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
       decoration: BoxDecoration(
-        color: Colors.transparent, // Transparent background
+        color: Colors.transparent,
         border: Border.all(
-          color: Colors.white, // White border color
-          width: 1.5, // Border width
+          color: Colors.white,
+          width: 1.5,
         ),
-        borderRadius: BorderRadius.circular(10.0), // Rounded corners
+        borderRadius: BorderRadius.circular(10.0),
       ),
       child: Text(
         title,
         style: TextStyle(
-          color: Colors.white, // White text color
+          color: Colors.white,
           fontSize: 16,
         ),
       ),
@@ -218,6 +183,265 @@ class GuideScreen extends StatelessWidget {
           'This is the User Guide Screen.',
           style: TextStyle(fontSize: 18.0),
         ),
+      ),
+    );
+  }
+}
+
+class ManagerLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFefefef),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/icon.png',
+                  width: 250,
+                  height: 250,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Manager Login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/manager_dashboard');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 40.0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 100.0,right: 100.0),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ManagerRegisterScreen()),
+                    );
+                  },
+                  child: Text(
+                    'Don\'t have an account? Register here.',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ManagerRegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFefefef),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/icon.png',
+                  width: 250,
+                  height: 250,
+                ),
+                Text(
+                  'Registraion',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 40.0),
+                  ),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ParticipantLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFefefef),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/icon.png',
+                  width: 250,
+                  height: 250,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Participant Login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/participant_dashboard');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 40.0),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ManagerDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manager Dashboard'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Manager Dashboard!'),
+      ),
+    );
+  }
+}
+
+class ParticipantDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Participant Dashboard'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Participant Dashboard!'),
       ),
     );
   }
