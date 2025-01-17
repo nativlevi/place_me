f57e04352879c8797842ac0b583186b65d84213b
nativlevi <nativl12345@gmail.com>
2025-01-17 08:44:24 +0200

    adding some style

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, '/login');
     });
 
